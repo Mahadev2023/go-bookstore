9e059bb30dcbe53df6d306f593f387d29b43583f
Mahadev <godbolemahadevkanaka@gmail.com>
2023-05-16 23:35:17 +0530

    first commit

---
+++ cmd/main/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/Mahadev2023/go-bookstore/pkg/routes"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	routes.RegisterBookStoreRoutes(r)
+
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:8000", r))
+
+}
